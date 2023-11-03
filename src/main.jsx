ab86491839325f289e5b129858df86c7663750e3
vbilan <vadym.bilan@neatbyte.solutions>
2023-11-03 10:49:30 +0200

    AI test were added

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,31 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+import { tryLoadAndStartRecorder } from '@alwaysmeticulous/recorder-loader'
+
+async function startApp() {
+    // Record all sessions on localhost, staging stacks and preview URLs
+    if (!isProduction()) {
+      // Start the Meticulous recorder before you initialise your app.
+      // Note: all errors are caught and logged, so no need to surround with try/catch
+      await tryLoadAndStartRecorder({
+        projectId: 'Gx3HocHD9DZL4cNMUwKOl1aIMB5zeeDKnbf6EjVP',
+        isProduction: false,
+      });
+    }
+
+    ReactDOM.createRoot(document.getElementById('root')).render(
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>,
+    )
+}
+
+function isProduction() {
+    // TODO: Update me with your production hostname
+    return window.location.hostname.indexOf("your-production-site.com") > -1;
+}
+
+startApp();
+
+
